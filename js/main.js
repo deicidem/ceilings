59955b2da13c5db2ac2d19e2ccdc4f1a4a1f5c09
deicidem <kronosdidi@gmail.com>
2019-06-21 20:50:34 +0300

    a

--- js/main.js
+++ js/main.js
@@ -1,22 +1,34 @@
 $(document).ready(function () {
+    let price = $('.calc-main__price');
+    let valArea = 0;
+    let valCount = 0;
+    let valColor = 0;
+    let valCover = 0;
+
     $('#area').range({
-        min: 0,
-        max: 20,
-        start: 5,
-        step: 5,
-        onChange: function (value, meta) {
-            if (meta.triggeredByUser) {
-                $('#area-value').html(value);
-            }
-        }
-    });
-    $('#count').range({
+        // Площадь потолка
+        // Здесь вы можете сзменить максимальное и минимальное значения для выбора, стартовое значение и шаг, с которым меняется значение
         min: 0,
         max: 100,
         start: 25,
         step: 1,
         onChange: function (value, meta) {
             if (meta.triggeredByUser) {
+                valArea = value;
+                $('#area-value').html(value);
+            }
+        }
+    });
+    $('#count').range({
+        // Количество углов и труб/шт
+        // Здесь вы можете сзменить максимальное и минимальное значения для выбора, стартовое значение и шаг, с которым меняется значение
+        min: 0,
+        max: 100,
+        start: 25,
+        step: 1,
+        onChange: function (value, meta) {
+            if (meta.triggeredByUser) {
+                valCount = value;
                 $('#count-value').html(value);
             }
         }
@@ -28,7 +40,7 @@ $(document).ready(function () {
 
     let tabs = $('.catalog-tab');
     let sliders = $('.catalog-slider');
-    sliders.hide()
+    sliders.hide();
     $('.catalog-slider_active').show();
     tabs.each(function (i) {
         $(this).on('click', () => {
@@ -43,5 +55,109 @@ $(document).ready(function () {
 
     $('.portfolio-item__text__desc').mCustomScrollbar({
         theme: "my-theme"
-    })
+    });
+
+    $("#cover").selectmenu({
+        change: () => {
+            valCover = +$("#cover").val();
+        }
+    });
+
+    $("#color").selectmenu({
+        change: () => {
+            valColor = +$("#color").val();
+        }
+    });
+
+    valColor = +$("#color").val();
+    valCover = +$("#cover").val();
+
+
+
+    $('.calc-main__button').on('click', function () {
+        // Ниже расположена формула для рассчета стоимость
+        // valArea - Площадь потолка, valCount - Количество углов и труб/шт, valColor - значение, соответствующее значению value в меню выбора цвета потолка, valCover - тоже самое что и с цветом.
+        let res = (valArea + valCover) * valCount;
+
+
+        price.html(res + " ₽");
+    });
+
+
+
+    $('.header-menu__nav a').on('click', function (event) {
+        event.preventDefault();
+        id = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(id).offset().top - 100
+        });
+    });
+
+
+    $('input[type="tel"]').mask('+7 (999) 999-99-99');
+
+    $('#main-form').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "php/main-form.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            $("#main-form").trigger("reset");
+            $('.popup').fadeIn();
+            setTimeout(() => {
+                $('.popup').fadeOut();
+            }, 5000);
+        });
+        return false;
+    });
+    $('#form').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "php/form.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            $("#form").trigger("reset");
+            $('.popup').fadeIn();
+            setTimeout(() => {
+                $('.popup').fadeOut();
+            }, 5000);
+        });
+        return false;
+    });
+    $('#offer-form').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "php/offer-form.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            $("#offer-form").trigger("reset");
+            $('.popup').fadeIn();
+            setTimeout(() => {
+                $('.popup').fadeOut();
+            }, 5000);
+        });
+        return false;
+    });
+    $('#question-form').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "php/question-form.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            $("#question-form").trigger("reset");
+            $('.popup').fadeIn();
+            setTimeout(() => {
+                $('.popup').fadeOut();
+            }, 5000);
+        });
+        return false;
+    });
 });
---
+++ php/form.php
@@ -0,0 +1,46 @@
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require 'vendor/autoload.php';
+
+// Instantiation and passing `true` enables exceptions
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+
+try {
+  $name = $_POST['name'];
+  $phone = $_POST['phone'];
+  $email = $_POST['email'];
+  //Server settings
+  $mail->SMTPDebug = 2;                                       // Enable verbose debug output
+  $mail->isSMTP();                                            // Set mailer to use SMTP // Specify main and backup SMTP servers
+  $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+  $mail->Host = "ssl://smtp.gmail.com";
+  $mail->Port = 465;
+  $mail->Username = "tolevelhost@gmail.com";
+  $mail->Password = "543bf72a1";                            // SMTP password
+  $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
+  $mail->setFrom('tolevelhost@gmail.com', 'xxx');
+  $mail->addAddress('
+nikolai.u.rogozin@gmail.com', 'Николай');     // Add a recipient
+
+  // Content
+  $mail->isHTML(true);                                  // Set email format to HTML
+  $mail->Subject = 'Заказ услуги с сайта xxx';
+
+  // Тело письма
+  $body = 'Пользователь заказал расчет стоимости <br>
+      Имя: ' . $name . ', <br>
+      Телефон: ' . $phone . ', <br>
+      E-mail: ' . $email . ' ';
+  $mail->msgHTML($body);
+  $mail->send();
+  return true;
+} catch (Exception $e) {
+  return false;
+}
